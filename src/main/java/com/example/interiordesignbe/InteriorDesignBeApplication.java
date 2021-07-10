55ba6c83898b7f502481d2a387e1a99b45485609
Vo Hong Son <vhson.fit.hcmus@gmail.com>
2021-07-10 23:12:56 +0700

    Init maser branch

---
+++ src/main/java/com/example/interiordesignbe/InteriorDesignBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.interiordesignbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InteriorDesignBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InteriorDesignBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/interiordesignbe/InteriorDesignBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.interiordesignbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InteriorDesignBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
